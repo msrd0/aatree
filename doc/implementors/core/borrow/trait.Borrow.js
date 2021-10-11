b4bfded036e8506eeb1b59dec9867aa97d8b36ed
msrd0 <msrd0@users.noreply.github.com>
2021-10-11 22:02:08 +0000

    Update Documentation for 9940af1209d5db4bf7b1c28aef59a6610cfb3bb7

---
+++ doc/implementors/core/borrow/trait.Borrow.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["aatree"] = [{"text":"impl&lt;K, V&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;K&gt; for <a class=\"struct\" href=\"aatree/struct.Entry.html\" title=\"struct aatree::Entry\">Entry</a>&lt;K, V&gt;","synthetic":false,"types":["aatree::map::Entry"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
