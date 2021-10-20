205eab76f03d35d53d31702c8dda74f82c666c86
msrd0 <msrd0@users.noreply.github.com>
2021-10-20 14:43:51 +0000

    Update Documentation for e7a3763fc4a818b28a4b10b2f5c6040f3870ba6c

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["aatree"] = {"name":"","dirs":[{"name":"node","files":["insert.rs","mod.rs","remove.rs","traverse.rs"]}],"files":["iter.rs","lib.rs","map.rs","set.rs"]};
+sourcesIndex["aatree"] = {"name":"","dirs":[{"name":"map","files":["entry.rs","get.rs","mod.rs"]},{"name":"node","files":["insert.rs","mod.rs","remove.rs","traverse.rs"]}],"files":["iter.rs","lib.rs","set.rs"]};
 createSourceSidebar();
