32ba5e51324bcc14a80cc81d8a568c0123db7324
Dominic <git@msrd0.de>
2022-10-13 16:34:13 +0200

    Add `append` function

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -7,7 +7,8 @@ use core::{
 	borrow::Borrow,
 	cmp::Ordering,
 	fmt::{self, Debug},
-	iter::FromIterator
+	iter::FromIterator,
+	mem
 };
 
 mod entry;
@@ -182,6 +183,41 @@ impl<K, V> AATreeMap<K, V> {
 			},
 			Some(entry) => Some(entry.value)
 		}
+	}
+
+	/// Moves all elements from `other` into `self`, leaving `other` empty.
+	///
+	/// # Examples
+	///
+	/// ```
+	/// use std::collections::BTreeMap;
+	///
+	/// let mut a = BTreeMap::new();
+	/// a.insert(1, "a");
+	/// a.insert(2, "b");
+	/// a.insert(3, "c");
+	///
+	/// let mut b = BTreeMap::new();
+	/// b.insert(3, "d");
+	/// b.insert(4, "e");
+	/// b.insert(5, "f");
+	///
+	/// a.append(&mut b);
+	///
+	/// assert_eq!(a.len(), 5);
+	/// assert_eq!(b.len(), 0);
+	///
+	/// assert_eq!(a[&1], "a");
+	/// assert_eq!(a[&2], "b");
+	/// assert_eq!(a[&3], "d");
+	/// assert_eq!(a[&4], "e");
+	/// assert_eq!(a[&5], "f");
+	/// ```
+	pub fn append(&mut self, other: &mut Self)
+	where
+		K: Ord
+	{
+		self.extend(mem::take(other));
 	}
 
 	/// Check if a key is contained within this map.
