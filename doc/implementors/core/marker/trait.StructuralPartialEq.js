bea521dd5a188e0371b78bc7373ec9e8ef2ee9e0
msrd0 <msrd0@users.noreply.github.com>
2022-12-02 22:52:47 +0000

    Update Documentation for 25f51ad17243f522f308fb56f269ec0179d03fdf

--- doc/implementors/core/marker/trait.StructuralPartialEq.js
+++ doc/implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["aatree"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"aatree/node/struct.AANode.html\" title=\"struct aatree::node::AANode\">AANode</a>&lt;T&gt;","synthetic":false,"types":["aatree::node::AANode"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"aatree":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"aatree/node/struct.AANode.html\" title=\"struct aatree::node::AANode\">AANode</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
